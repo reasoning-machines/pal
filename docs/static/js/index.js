79569c7f0ee08f7205874bb61e51cba639a320d6
alexisxy <alexisxy0418@gmail.com>
2022-11-22 14:04:52 -0500

    add interactive examples

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -9,4 +9,34 @@ $(document).ready(function() {
       $(".navbar-menu").toggleClass("is-active");
 
     });
+
+    var current_cmd_idxs = {
+        "gsm": 1,
+        "gsmhard": 1,
+        "coloredobjects":1,
+        "repeatcopy":1,
+        "date":1
+    }
+
+    // examples
+    $('select').on('change', function() {
+        var sep_idx = this.value.indexOf('_');
+        var domain_name = this.value.substring(0, sep_idx);
+        var desired_cmd_idx = parseInt(this.value.substring(sep_idx + 1));
+        var current_cmd_idx = current_cmd_idxs[domain_name];
+
+        // hide current content
+        var current_content = $('#content_' + domain_name + "_" + current_cmd_idx.toString());
+        current_content.hide();
+
+        // show desired content
+        var desired_content = $('#content_' + domain_name + "_" + desired_cmd_idx.toString());
+        desired_content.show();
+
+        // set current to desired
+        current_cmd_idxs[domain_name] = desired_cmd_idx;
+    });
 })
+
+
+
