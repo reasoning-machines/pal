75eed60d0e929cb3cd862e96dbe0b664e505a9a1
urialon <urialon1@gmail.com>
2023-03-17 14:23:24 -0400

    adding support for gpt-3.5 and gpt-4

--- pal/core/backend.py
+++ pal/core/backend.py
@@ -24,11 +24,13 @@ def call_gpt(prompt, model='code-davinci-002', stop=None, temperature=0., top_p=
     num_completions = majority_at if majority_at is not None else 1
     num_completions_batch_size = 5
     
+        
     completions = []
     for i in range(20 * (num_completions // num_completions_batch_size + 1)):
         try:
             requested_completions = min(num_completions_batch_size, num_completions - len(completions))
-            ans = openai.Completion.create(
+            if model.startswith('gpt-4') or model.startswith('gpt-3.5-turbo'):
+                ans = chat_api(
                             model=model,
                             max_tokens=max_tokens,
                             stop=stop,
@@ -37,9 +39,46 @@ def call_gpt(prompt, model='code-davinci-002', stop=None, temperature=0., top_p=
                             top_p=top_p,
                             n=requested_completions,
                             best_of=requested_completions)
-            completions.extend([choice['text'] for choice in ans['choices']])
+            else:
+                ans = completions_api(
+                            model=model,
+                            max_tokens=max_tokens,
+                            stop=stop,
+                            prompt=prompt,
+                            temperature=temperature,
+                            top_p=top_p,
+                            n=requested_completions,
+                            best_of=requested_completions)
+            completions.extend(ans)
             if len(completions) >= num_completions:
                 return completions[:num_completions]
         except openai.error.RateLimitError as e:
             time.sleep(min(i**2, 60))
     raise RuntimeError('Failed to call GPT API')
+
+def completions_api(model, max_tokens, stop, prompt, temperature,
+            top_p, n, best_of):
+    ans = openai.Completion.create(
+        model=model,
+        max_tokens=max_tokens,
+        stop=stop,
+        prompt=prompt,
+        temperature=temperature,
+        top_p=top_p,
+        n=n,
+        best_of=best_of)
+    return [choice['text'] for choice in ans['choices']]
+
+def chat_api(model, max_tokens, stop, prompt, temperature,
+            top_p, n, best_of):
+    ans = openai.ChatCompletion.create(
+        model=model,
+        max_tokens=max_tokens,
+        stop=stop,
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful assistant that can write Python code that solves mathematical reasoning questions similarly to the examples that you will be provided.'},
+            {'role': 'user', 'content': prompt}],
+        temperature=temperature,
+        top_p=top_p,
+        n=n)
+    return [choice['message']['content'] for choice in ans['choices']]
